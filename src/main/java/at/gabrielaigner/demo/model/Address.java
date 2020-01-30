2623f85edc4dd3936caccfcd2bf50607f61ae454
gabrielaigner <gabsi.aigner@gmail.com>
2020-01-30 17:22:05 +0100

    Add Column Annotations in Address & Person

--- src/main/java/at/gabrielaigner/demo/model/Address.java
+++ src/main/java/at/gabrielaigner/demo/model/Address.java
@@ -2,6 +2,29 @@ package at.gabrielaigner.demo.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-public class Address extends PanacheEntity {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "MY_ADDRESS")
+public class Address extends PanacheEntity {
+    @Column(name = "A_STREET")
+    String street;
+
+    @Column(name = "A_CITY")
+    String city;
+
+    @Column(name = "A_ZIP_CODE")
+    int zipCode;
+
+    @ManyToOne @JoinColumn(name = "A_PERSON_ID")
+    Person person;
+
+    public Address() {
+    }
+
+    public Address(String street, String city, int zipCode){
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
 }
--- src/main/java/at/gabrielaigner/demo/model/Person.java
+++ src/main/java/at/gabrielaigner/demo/model/Person.java
@@ -3,9 +3,11 @@ package at.gabrielaigner.demo.model;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "MY_PERSON")
 public class Person extends PanacheEntity {
     public String name;
     public LocalDate birth;
