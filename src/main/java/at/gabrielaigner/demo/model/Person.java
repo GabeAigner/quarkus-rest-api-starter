584a905992ff407e598b9e63512b75223b2b637b
gabrielaigner <gabsi.aigner@gmail.com>
2020-01-30 16:52:45 +0100

    Add Person

---
+++ src/main/java/at/gabrielaigner/demo/model/Person.java
@@ -0,0 +1,22 @@
+package at.gabrielaigner.demo.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Person extends PanacheEntity {
+    public String name;
+    public LocalDate birth;
+    public String status;
+
+    public Person() {
+    }
+
+    public Person(String name, LocalDate birth, String status) {
+        this.name = name;
+        this.birth = birth;
+        this.status = status;
+    }
+}
