bdbc122aad351ff544c9132efe88e694614cd109
gabrielaigner <gabsi.aigner@gmail.com>
2020-01-30 15:55:51 +0100

    Initial commit

---
+++ src/test/java/at/gabrielaigner/demo/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package at.gabrielaigner.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
